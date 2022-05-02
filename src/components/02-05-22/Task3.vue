205c11c5f400ae0ccce007ac36f5f89cda036117
Abhi-500apps <abhishek.kudupudi@500apps.com>
2022-05-02 18:42:08 +0530

    task(02-05-22)

---
+++ src/components/02-05-22/Task3.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>This is question-3</h1>
+
+    <button @click="remove()">Click!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AbhI",
+  data(){
+    return {
+      array: [1, 1, 2, 2, 2, 3, 4, 3, 2],
+    }
+  },
+  methods: {
+    remove() {
+      console.log((this.array = [...new Set(this.array)]));
+    },
+  },
+};
+</script>
---
+++ src/components/02-05-22/Task5.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>question-5</h1>
+    <button @click="fun()">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AbhI",
+  data() {
+    return {
+      myColor: ["Red", "Green", "White", "Black"],
+      res:" "
+    };
+  },
+  methods: {
+    fun() {
+      this.res=console.log(this.myColor.toString())+console.log(this.myColor.join(' '))+console.log(this.myColor.join('+'));
+    },
+  },
+};
+</script>
