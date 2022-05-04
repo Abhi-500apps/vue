a277ca42d1a2f047383aec468da09db534d77077
Abhi-500apps <abhishek.kudupudi@500apps.com>
2022-05-04 19:17:14 +0530

    task(04-05-22)

---
+++ src/components/04-05-22/Task4.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <h1>Question-4</h1>
+      <button @click="fun()">click</button>
+      </div>
+</template>
+<script>
+export default {
+    name:"AbhI",
+    data(){
+        return{
+            res:" ",
+
+        }
+    },
+    methods:{
+        fun(){
+            function isArray(input){
+
+            return !!input.pop;
+            }
+            this.res=
+            console.log(isArray(''));
+            console.log(isArray([1, 2, 4, 0]));
+
+        }
+    }
+
+}
+</script>
